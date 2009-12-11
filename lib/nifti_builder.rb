81b4d75b37c236acbd7e9dd0f972c69d70d5ef70
Erik Kastman <erik@nelson.medicine.wisc.edu>
2009-12-11 12:30:31 -0600

    Missed nifti_builder file and test on last commit - trouble with git stash...

---
+++ lib/nifti_builder.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+=begin rdoc
+Builds Nifti files from Dicoms.
+=end
+
+TO3D_CMD = 'to3d'
+
+module UnknownImageDataset
+  def dataset_to_nifti(nifti_output_directory, nifti_filename, input_options = {} )
+    if input_options.has_key?(:dicom_files)
+      input_files = input_options[:dicom_files].each {|file| file.to_s }.join(' ')
+    elsif input_options.has_key?(:input_directory)
+      input_files = "#{input_options[:input_directory]}/'#{glob}'"
+    else input_files = "#{Dir.tmpdir}/'#{glob}'"
+    end
+    
+    nifti_output_directory = File.join(nifti_output_directory, 'unknown') if input_options[:append_modality_directory]
+    nifti_output_file = File.join(nifti_output_directory, nifti_filename)
+    
+    File.makedirs(nifti_output_directory) unless File.directory?(nifti_output_directory)
+    raise(IOError, "Cannot write to #{nifti_output_directory}") unless File.writable?(nifti_output_directory)
+        
+    nifti_conversion_command = "#{TO3D_CMD} -session #{nifti_output_directory} -prefix #{nifti_filename} #{input_files}"
+
+    return nifti_conversion_command, nifti_output_file
+  end
+end
---
+++ test/nifti_builder_spec.rb
@@ -0,0 +1,85 @@
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+
+require 'rubygems'
+require 'spec'
+require 'visit_raw_data_directory'
+require 'raw_image_dataset'
+require 'raw_image_file'
+
+describe "Convert Unknown Dicoms to Nifti Files" do
+  
+  before(:each) do
+    @visit = VisitRawDataDirectory.new('/Data/vtrak1/raw/johnson.tbi-va.visit1/tbiva018b_9336_12022009', 'johnson.tbi-va.visit1')
+    @dataset = RawImageDataset.new('/Data/vtrak1/raw/johnson.tbi-va.visit1/tbiva018b_9336_12022009/001', [RawImageFile.new('/Data/vtrak1/raw/johnson.tbi-va.visit1/tbiva018b_9336_12022009/001/I0001.dcm')])
+    @test_niftis = Array.new
+    @output_directories = Array.new
+  end
+
+  it "should Convert an anatomical from dicom to nifti using original, unzipped files." do
+    @dataset.to_nifti('/tmp/', 'filename.nii', :input_directory => '/Data/vtrak1/raw/asthana.adrc-clinical-core.visit1/adrc00001_294_12022009/001')[0].should == "to3d -session /tmp/ -prefix filename.nii /Data/vtrak1/raw/asthana.adrc-clinical-core.visit1/adrc00001_294_12022009/001/'*.dcm'"
+    nifti_conversion_command, nifti_output_file = @dataset.to_nifti!('/tmp/', 'filename.nii', :input_directory => '/Data/vtrak1/raw/asthana.adrc-clinical-core.visit1/adrc00001_294_12022009/001')
+    nifti_conversion_command.should == "to3d -session /tmp/ -prefix filename.nii /Data/vtrak1/raw/asthana.adrc-clinical-core.visit1/adrc00001_294_12022009/001/'*.dcm'"
+    @test_niftis << nifti_output_file
+    @output_directories << '/tmp'
+  end
+  
+  it "should convert all anatomicals in a visit raw directory using original, unzipped files." do
+    @visit.scan
+    
+    @visit.datasets.each do |ds|
+      begin 
+        nifti_filename = "#{@visit.scanid}_#{ds.escape_filename(ds.series_description)}_#{File.basename(ds.directory)}.nii"
+        nifti_conversion_commmand, nifti_output_file = ds.to_nifti!(File.join(Dir.tmpdir, @visit.default_preprocess_directory), nifti_filename, :input_directory => @dataset.directory, :append_modality_directory => true )
+        @test_niftis << nifti_output_file
+      rescue IOError => e
+        puts "-- Error: #{e.message}"
+      end
+    end
+    
+    @output_directories = File.join(@visit.default_preprocess_directory, 'unknown')
+  end
+  
+  it "should convert all anatomicals in a visit raw directory using local copy." do
+    @test_niftis << @visit.to_nifti!('/tmp')
+  end
+  
+  it "should create a good to3d command with input directory or dicom files." do 
+    nifti_output_path = '/tmp'
+    nifti_filename = '001.nii'
+    dataset_pathname = Pathname.new(@dataset.directory)
+    dataset_pathname.all_dicoms do |dicom_files| 
+      nifti_conversion_command, nifti_output_file = @dataset.to_nifti(nifti_output_path, nifti_filename)
+      nifti_conversion_command.should == "to3d -session /tmp -prefix 001.nii #{Dir.tmpdir}/'*.dcm'"
+
+      
+      nifti_conversion_command, nifti_output_file = @dataset.to_nifti(nifti_output_path, nifti_filename, :input_directory => '/tmp')
+      nifti_conversion_command.should == "to3d -session /tmp -prefix 001.nii /tmp/'*.dcm'"
+
+      
+      nifti_conversion_command, nifti_output_file = @dataset.to_nifti(nifti_output_path, nifti_filename, :dicom_files => dicom_files)
+      nifti_conversion_command.should == "to3d -session /tmp -prefix 001.nii #{Dir.tmpdir}/I0001.dcm #{Dir.tmpdir}/I0002.dcm #{Dir.tmpdir}/I0003.dcm #{Dir.tmpdir}/I0004.dcm #{Dir.tmpdir}/I0005.dcm #{Dir.tmpdir}/I0006.dcm #{Dir.tmpdir}/I0007.dcm #{Dir.tmpdir}/I0008.dcm #{Dir.tmpdir}/I0009.dcm #{Dir.tmpdir}/I0010.dcm #{Dir.tmpdir}/I0011.dcm #{Dir.tmpdir}/I0012.dcm #{Dir.tmpdir}/I0013.dcm #{Dir.tmpdir}/I0014.dcm #{Dir.tmpdir}/I0015.dcm"
+
+    end
+  end
+  
+  it "should append a modality if the :append_modality_directory is true." do
+    # For an unknown modality:
+    nifti_conversion_command, nifti_output_file = @dataset.to_nifti(Dir.tmpdir, 'filename.nii', :append_modality_directory => true)
+    nifti_conversion_command.should == "to3d -session #{Dir.tmpdir}/unknown -prefix filename.nii #{Dir.tmpdir}/'*.dcm'"
+  end
+  
+  it "should guess scan id" do
+    @visit.scanid.should == 'tbiva018b'
+    
+    v = VisitRawDataDirectory.new('/Data/vtrak1/preprocessed/visits/asthana.adrc-clinical-core.visit1/adrc00001', 'asthana.adrc-clinical-core.visit1')
+    v.scanid.should == 'adrc00001'
+  end
+  
+  after do
+    @test_niftis.flatten.each { |nifti| File.delete(nifti) } unless @test_niftis.empty?
+    [@output_directories, Dir.tmpdir, '/tmp'].flatten.each do |temp_dir|
+      Dir.foreach(temp_dir) {|f| File.delete(File.join(temp_dir, f)) if File.extname(f) == '.nii'}
+    end
+  end
+  
+end
